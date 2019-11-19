bc7d2def9cb866cbf8b5d5b1014210b428126bae
Guhan S <37925679+Guhan2211@users.noreply.github.com>
2019-11-20 01:01:47 +0530

    Add files via upload

---
+++ library/thing.ino
@@ -0,0 +1,101 @@
+#include "ESP8266WiFi.h"
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include "ThingSpeak.h"
+
+// WiFi parameters to be configured
+const char* ssid = "Thilaga";
+const char* password = "99019307";
+int RXPin = 4;
+int TXPin = 5;
+int GPSBaud = 9600;
+
+unsigned long myChannelNumber = 600627;
+const char * myWriteAPIKey = "VPJIUIG40BV2JPNQ";
+
+
+WiFiClient  client;
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void setup(void)
+{ 
+  Serial.begin(9600);
+  gpsSerial.begin(GPSBaud);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+  ThingSpeak.begin(client);
+  
+
+  
+
+}
+void loop() {
+ 
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println("No GPS detected");
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+
+  //uploading stuff
+
+
+  
+    double latitude = (gps.location.lat());
+    double longitude = (gps.location.lng());
+    
+    String latbuf=String(latitude,6);
+    String lonbuf=String(longitude,6);
+    
+          
+                        ThingSpeak.setField(1, latbuf);
+                        ThingSpeak.setField(2, lonbuf);
+                        int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+                        if(x == 200){
+                            Serial.println("Channel update successful.");
+                                      }
+                        else{
+                            Serial.println("Problem updating channel. HTTP error code " + String(x));
+                             }
+                                                         
+  
+       
+       
+  }
+ 
+
+ else
+  {
+    Serial.println("Location: Not Available");
+  }
+      delay(10000);
+      Serial.println();
+  }
+ 
+  
+ 
+
+
+
